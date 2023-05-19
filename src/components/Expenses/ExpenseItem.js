6f9c2cdfa9a352c0f2b34b571f87dcea3b47c2d4
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-05-19 11:03:37 +0300

    d

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,13 @@
 import ExpenseDate from './ExpenseDate';
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
+
+
 function ExpenseItem(props) {
     
+    function changeTitle(){
+        props.title="Titlc changed on clik"
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={ props.date} />
@@ -10,10 +15,11 @@ function ExpenseItem(props) {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={changeTitle}>Change Title</button>
         </Card>
     );
 }
 
 
 
-export default ExpenseItem;
+export default ExpenseItem; 
