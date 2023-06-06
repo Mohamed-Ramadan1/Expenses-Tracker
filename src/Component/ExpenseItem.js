f59e59bd3a04d6823c12f2321ba5e0d3e9fbe5f5
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-06 09:32:13 +0300

    create spence date

--- src/Component/ExpenseItem.js
+++ src/Component/ExpenseItem.js
@@ -1,17 +1,16 @@
-import React from "react";
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
 
-const ExpenseItem = () => {
-    const expenceDate = new Date(2021, 2, 28);
-    const expenceTitle = "Car Insurance";
-    const expencAmount = 294.67;
+
+const ExpenseItem = (props) => {
+    const { title, amount, date } = props;
     return (
         <div className="expense-item ">
-            <div>{expenceDate.toISOString()}</div>
+            <ExpenseDate date={date}/>
 
             <div className="expense-item__description">
-                <h2 className="expense-item h2">{expenceTitle}</h2>
-                <div className="expense-item__price">${expencAmount}</div>
+                <h2 className="expense-item h2">{title}</h2>
+                <div className="expense-item__price">${amount}</div>
             </div>
 
         </div>
